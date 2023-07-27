fce624976b2c0b843a5a9a9e88c99caf7b5fc1ae
Ivan Usatyi <usavano@Oryna-MacBook-Pro.local>
2023-07-27 11:08:59 +0300

    refactoring render, update functions. Add info row when data is empty

--- src/app.js
+++ src/app.js
@@ -19,9 +19,18 @@ let generalNotesArray = [];
 let statArray = createStatArray(notesArray);
 const queryMainTable = 'notesContainer';
 const queryStatTable = 'statisticContainer';
+const queryArchiveTable = 'notesArchiveContainer';
 const createNoteBtn = document.querySelector('.createNoteBtn');
 const popUpWinEdit = document.querySelector('.popUpWin-edit');
 const editFormBtn = popUpWinEdit.querySelector('.editForm-btn');
+
+const infoRow = document.createElement('tr');
+infoRow.classList.add('infoRow');
+infoRow.setAttribute('id', 'infoRowId');
+infoRow.innerHTML = `
+<td colspan="6">Theres is no items </td>
+</tr>`;
+
 let currentEditableElementKey = null;
 
 // appoint keys to notes
@@ -34,6 +43,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const renderStats = renderData(queryStatTable, statArray, createStatRow);
   renderStats();
+
+  const renderArchiveNotes = renderData(
+    queryArchiveTable,
+    notesAchivedArray,
+    createArchiveNote
+  );
+  renderArchiveNotes();
 });
 createNoteBtn.addEventListener('click', () => {
   handleCreateNote();
@@ -112,17 +128,60 @@ function createStatRow(categoryObj) {
 
   return statEl;
 }
+// creating single arhive note
+function createArchiveNote(archNoteObj) {
+  const { key, name, date, category, content } = archNoteObj;
+
+  const maxNameLength = 20;
+  const maxContentLength = 25;
+  const formatedName = sliceLongText(name, maxNameLength);
+  const formatedContent = sliceLongText(content, maxContentLength);
+
+  const innerDates = gatherDatesInText(content);
+
+  const noteArchiveElement = document.createElement('tr');
+  const attr = document.createAttribute('data-key');
+  attr.value = key;
+  noteArchiveElement.setAttributeNode(attr);
+
+  noteArchiveElement.classList.add('archiveNote');
+
+  noteArchiveElement.innerHTML = `
+    <td class="noteArchiveName td-archive">${formatedName}</td>
+    <td class="noteArchiveCreationDate td-archive">${
+      months[date.getMonth()]
+    } ${date.getDate()}, ${date.getFullYear()}</td>
+    <td class="noteArchiveCategory td-archive">${
+      category === 'randomThought' ? 'random thought' : category
+    }</td>
+    <td class="noteArchiveContent td-archive">${formatedContent}</td>
+    <td class="noteArchiveDates td-archive">${innerDates}</td>
+    <td class="noteArchiveActions td-archive">
+        <button class="btnUnarchive btn">Unarchive</button>
+    </td>`;
+
+  const unarchiveBtn = noteArchiveElement.querySelector('.btnUnarchive');
+  unarchiveBtn.addEventListener('click', (event) => {
+    unarchiveNote(event);
+  });
+
+  return noteArchiveElement;
+}
 // rendering data
 function renderData(qContainer, data, createFn) {
   return () => {
     const dataContainer = document.querySelector(`.${qContainer}`);
 
-    data.forEach((obj) => {
-      renderElement(dataContainer, obj, createFn);
-    });
+    if (data.length === 0) {
+      dataContainer.appendChild(infoRow);
+    } else {
+      data.forEach((obj) => {
+        renderElement(dataContainer, obj, createFn);
+      });
+    }
   };
 }
-// delete notes
+// handle delete Note
 function deleteNote(e) {
   const parent = e.currentTarget.parentElement.parentElement;
   const parentKey = parent.getAttribute('data-key');
@@ -209,9 +268,21 @@ function handleEditNote(e) {
   );
   updateMainTableData();
 
+  statArray =
+    generalNotesArray.length > 0
+      ? createStatArray(generalNotesArray)
+      : createStatArray(notesArray);
+
+  const updateStatTableData = updateDataOnPage(
+    statArray,
+    queryStatTable,
+    createStatRow
+  );
+  updateStatTableData();
+
   popUpWinEdit.classList.remove('active');
 }
-
+// handle archive Note
 function archiveNote(e) {
   const parent = e.currentTarget.parentElement.parentElement;
   const parentKey = parent.getAttribute('data-key');
@@ -228,7 +299,57 @@ function archiveNote(e) {
       notesAchivedArray.push(note);
     }
   });
+
   notesArray = notesActiveArray;
+  const updateMainTableData = updateDataOnPage(
+    notesArray,
+    queryMainTable,
+    createNote
+  );
+  updateMainTableData();
+
+  const updateArchiveTableData = updateDataOnPage(
+    notesAchivedArray,
+    queryArchiveTable,
+    createArchiveNote
+  );
+  updateArchiveTableData();
+
+  generalNotesArray = notesArray.concat(notesAchivedArray);
+  statArray = createStatArray(generalNotesArray);
+  const updateStatTableData = updateDataOnPage(
+    statArray,
+    queryStatTable,
+    createStatRow
+  );
+  updateStatTableData();
+}
+// handle unarchive Note
+function unarchiveNote(e) {
+  const parent = e.currentTarget.parentElement.parentElement;
+  const parentKey = parent.getAttribute('data-key');
+  notesAchivedArray.forEach((note) => {
+    if (note.key === parseInt(parentKey)) {
+      note.archiveStatus = false;
+    }
+  });
+
+  const notesUnactiveArray = notesAchivedArray.filter((note) => {
+    if (note.archiveStatus) {
+      return note;
+    } else {
+      notesArray.push(note);
+    }
+  });
+
+  notesAchivedArray = notesUnactiveArray;
+  const updateArchiveTableData = updateDataOnPage(
+    notesAchivedArray,
+    queryArchiveTable,
+    createArchiveNote
+  );
+  updateArchiveTableData();
+
   const updateMainTableData = updateDataOnPage(
     notesArray,
     queryMainTable,
@@ -244,9 +365,7 @@ function archiveNote(e) {
     createStatRow
   );
   updateStatTableData();
-  console.log(notesAchivedArray);
 }
-
 // handle create Note
 async function handleCreateNote() {
   nullInputs();
@@ -263,7 +382,11 @@ async function handleCreateNote() {
     );
     updateMainTableData();
 
-    statArray = createStatArray(notesArray);
+    generalNotesArray = notesArray.concat(notesAchivedArray);
+    statArray =
+      generalNotesArray.length > 0
+        ? createStatArray(generalNotesArray)
+        : createStatArray(notesArray);
 
     const updateStatTableData = updateDataOnPage(
       statArray,
@@ -281,8 +404,12 @@ function updateDataOnPage(data, queryContainer, createFn) {
     const tableContainer = document.querySelector(`.${queryContainer}`);
     tableContainer.innerHTML = '';
 
-    data.forEach((item) => {
-      renderElement(tableContainer, item, createFn);
-    });
+    if (data.length === 0) {
+      tableContainer.appendChild(infoRow);
+    } else {
+      data.forEach((item) => {
+        renderElement(tableContainer, item, createFn);
+      });
+    }
   };
 }
